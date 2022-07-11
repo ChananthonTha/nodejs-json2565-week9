64b6901e1eb63a8ae9f7bc8cf269537ae8aa2a0b
ชนันธร ถนอมเงิน <koonzee14@gmail.com>
2022-07-11 17:33:19 +0700

    up

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const http = require('http')
 const msg = require('./message.js')
-const {loadUser} = require('./user_model')
+// const {loadUser} = require('./user_model')
+const {loadVnl2022} = require('./vnl2022_model')
 
 const host = "localhost"
 const PORT = process.env.PORT || 5000
 
 const server = http.createServer((req,res) => {
-    const jsonContent = JSON.stringify(loadUser())
-
+    // const jsonContent = JSON.stringify(loadUser())
+    const jsonContent = JSON.stringify(importVnl2022())
     res.writeHead(200, { 'Content-Type' : 'text/plain' })
     res.write(`${jsonContent}`)
     res.end()
