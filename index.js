c99eca76bb0dac97da0c31b83d3970124675da5c
ชนันธร ถนอมเงิน <koonzee14@gmail.com>
2022-07-11 16:45:00 +0700

    up

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const http = require('http')
 const msg = require('./message.js')
 const {loadUser} = require('./user_model')
+
 const host = "localhost"
 const PORT = process.env.PORT || 5000
 
 const server = http.createServer((req,res) => {
     const jsonContent = JSON.stringify(loadUser())
+
     res.writeHead(200, { 'Content-Type' : 'text/plain' })
     res.write(`${msg.message}`)
     res.end()
