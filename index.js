e4fb78bfa9c4fd7f52ea7017b04b6ab4a4ccf0ec
ชนันธร ถนอมเงิน <koonzee14@gmail.com>
2022-07-11 16:15:29 +0700

    up

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const http = require('http')
-const msg = require('./message.js')
+const msg = require('message.js')
 
 const host = "localhost"
 const PORT = process.env.PORT || 5000
+
 const server = http.createServer((req,res) => {
     res.writeHead(200, {'Content-Type':'text/plain'})
-    res.write(`${msg.message} Lungtoo`)
+    res.write(`${msg.message}`)
     res.end()
 })
 
